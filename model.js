5fd5514845bee2e560df5aa2bbf299c7e9a287b7
emily-bradfield <emily-bradfield@outlook.com>
2023-01-07 23:08:18 +0000

    documentation

--- model.js
+++ model.js
@@ -1,8 +1,20 @@
+/**
+ * @module model.js
+ * @exports createSchema() function for creating a mongoose.Schema()
+ * 
+ * Defines a mongoose.Schema model given an array of key-value pairs and writes the resulting model to another file to be called as needed
+ */
+
 const fs = require("fs")
 
 const MONGOOSE = require("mongoose");
 const {Schema, model} = MONGOOSE;
 
+/**
+ * Creates a new mongoose.Schema() from @param SCHEMA_JSON and writes the code define and export the model to the file documentModel.js
+ * 
+ * @param {String} SCHEMA_JSON A stringified JSON Object containing key : value pairs of attributes of the document 
+ */
 const writeSchema = (SCHEMA_JSON) => {
 let CONTENT = `const MONGOOSE = require('mongoose'); \nconst {Schema, model} = MONGOOSE; \n\nconst documentSchema = new Schema(${SCHEMA_JSON})\n\nconst Document = model("Document", documentSchema)\nmodule.exports = {"Document": Document}`
     fs.writeFile(`./documentModel.js`, CONTENT, err => {
@@ -10,6 +22,11 @@ let CONTENT = `const MONGOOSE = require('mongoose'); \nconst {Schema, model} = M
     })
 };
 
+/**
+ * Creates a stringified JSON object based on @param SCHEMA_ARRAY and passes it to the writeSchema() function 
+ * 
+ * @param {Array} SCHEMA_ARRAY An array containing key : value pairs as strings, each describing an attribute of the document as name : type
+ */
 exports.createSchema = function(SCHEMA_ARRAY){
     let STRING = "{"
     for (let i = 0; i < SCHEMA_ARRAY.length; i++) {
--- router.js
+++ router.js
@@ -1,17 +1,32 @@
+/**
+ * @module router.js
+ * @exports ROUTER an express.Router() object
+ * @exports createRoutes a function to create a set of 5 default/pre-determined routes
+ * @exports customRoute a function to create a custom route
+ */
 const fs = require("fs");
 
 const AXIOS = require("axios");
 const ROUTER = require('express').Router();
 const {Document} = require("./documentModel")
 
+/**
+ * @function createRoutes creates five default express.Router() routes for basic CRUD functionality
+ */
 const createRoutes = () => {
 
+    /**
+     * @summary CREATE (POST)
+     */
     ROUTER.route('/create').post((req, res) => {
         let OBJECT = req.body;
         const NEW_OBJECT = new Document(OBJECT);
         NEW_OBJECT.save().then(() => res.json(NEW_OBJECT)).catch((err) => res.json(err))
     })
 
+    /**
+     * @summary READ (GET)
+     */
     ROUTER.route('/read').get((req, res) => {
         Document.find().then((documents) => res.json(documents)).catch((err) => res.json(err))
     })
@@ -20,15 +35,28 @@ const createRoutes = () => {
         Document.findById(req.params._id).then((document) => res.json(document)).catch((err) => res.json(err))
     })
 
+    /**
+     * @summary UPDATE (PUT/POST)
+     */
     ROUTER.route('/update/:_id').post((req, res) => {
         Document.findByIdAndUpdate({'_id':req.params._id}, req.params.body).then(() => res.json(true)).catch((err) => res.json(err))
     })
 
+    /**
+     * @summary DELETE (DELETE)
+     */
     ROUTER.route('/delete/:_id').delete((req, res) => {
         Document.findByIdAndDelete(req.params._id).then(() => res.json(true)).catch((err) => res.json(err))
     })
 }
 
+/**
+ * Creates a customer route given the type of request, the url for the route, and a parameter (optional)
+ * 
+ * @param {String} TYPE the type of HTTP reuqest to make. Can be 'post', 'get', 'put', or 'delete'
+ * @param {String} ROUTE_URL the route to direct the HTTP request to
+ * @param {String} PARAM only required for readOne, update, and delete; the attribute by which to match any given document to 
+ */
 const customRoute = (TYPE, ROUTE_URL, PARAM = null) => {
     switch (TYPE){
         case 'post':
