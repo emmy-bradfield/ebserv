b07ff3d1f895a0e3a34552c012a7b054f870b540
emily-bradfield <emily-bradfield@outlook.com>
2023-01-07 21:04:23 +0000

    server.js functional and fully integrated

--- router.js
+++ router.js
@@ -1,68 +1,65 @@
 const fs = require("fs");
 
 const AXIOS = require("axios");
-const { resolveAny } = require("dns");
-const { Model } = require("mongoose");
 const ROUTER = require('express').Router();
+const {Document} = require("./documentModel")
 
-const createRoutes = (SCHEMA_NAME, SCHEMA_PATH) => {
-    const {Model} = require(`${SCHEMA_PATH}/${SCHEMA_NAME}Model.js`)
+const createRoutes = () => {
 
     ROUTER.route('/create').post((req, res) => {
         let OBJECT = req.body;
-        const NEW_OBJECT = new Model(OBJECT);
+        const NEW_OBJECT = new Document(OBJECT);
         NEW_OBJECT.save().then(() => res.json(NEW_OBJECT)).catch((err) => res.json(err))
     })
 
     ROUTER.route('/read').get((req, res) => {
-        Model.find().then((documents) => res.json(documents)).catch((err) => res.json(err))
+        Document.find().then((documents) => res.json(documents)).catch((err) => res.json(err))
     })
 
     ROUTER.route('/read/:_id').get((req, res) => {
-        Model.findById(req.params._id).then((document) => res.json(document)).catch((err) => res.json(err))
+        Document.findById(req.params._id).then((document) => res.json(document)).catch((err) => res.json(err))
     })
 
     ROUTER.route('/update/:_id').post((req, res) => {
-        Model.findByIdAndUpdate({'_id':req.params._id}, req.params.body).then(() => res.json(true)).catch((err) => res.json(err))
+        Document.findByIdAndUpdate({'_id':req.params._id}, req.params.body).then(() => res.json(true)).catch((err) => res.json(err))
     })
 
     ROUTER.route('/delete/:_id').delete((req, res) => {
-        Model.findByIdAndDelete(req.params._id).then(() => res.json(true)).catch((err) => res.json(err))
+        Document.findByIdAndDelete(req.params._id).then(() => res.json(true)).catch((err) => res.json(err))
     })
 }
 
-const customRoute = (SCHEMA_NAME, SCHEMA_PATH, TYPE, ROUTE_URL, PARAM = null) => {
-    const {Model} = require(`${SCHEMA_PATH}/${SCHEMA_NAME}Model.js`)
+const customRoute = (Document, TYPE, ROUTE_URL, PARAM = null) => {
     switch (TYPE){
         case 'post':
             ROUTER.route(ROUTE_URL).post((req, res) => {
                 let OBJECT = req.body
-                const NEW_OBJECT = new Model(OBJECT);
+                const NEW_OBJECT = new Document(OBJECT);
                 NEW_OBJECT.save().then(() => res.json(NEW_OBJECT)).catch((err) => res.json(err))
             });
             break;
         case 'get':
             if (PARAM) {
                 ROUTER.route(`${ROUTE_URL}/:${PARAM}`).get((req, res) => {
-                    Model.findOne({PARAM : req.params.PARAM}).then((document) => res.json(document)).catch((err) => res.json(err))
+                    Document.findOne({PARAM : req.params.PARAM}).then((document) => res.json(document)).catch((err) => res.json(err))
                 })
             } else {
                 ROUTER.route(ROUTE_URL).get((req, res) => {
-                    Model.find().then((documents) => res.json(documents)).catch((err) => res.json(err))
+                    Document.find().then((documents) => res.json(documents)).catch((err) => res.json(err))
                 })
             };
             break;
         case 'put':
             if (PARAM){
                 ROUTER.route(ROUTE_URL).post((req, res) => {
-                    Model.updateOne({PARAM: req.params.PARAM}, req.body).then(() => res.json(true)).catch((err) => res.json(err))
+                    Document.updateOne({PARAM: req.params.PARAM}, req.body).then(() => res.json(true)).catch((err) => res.json(err))
                 })
             } else {throw new Error("MissingParamError: A parameter is required for put functions")};
             break;
         case 'delete':
             if (PARAM){
                 ROUTER.route(ROUTE_URL).delete((req, res) => {
-                    Model.findOneAndDelete({PARAM:req.params.PARAM}).then(() => res.json(true)).catch((err) => res.json(err))
+                    Document.findOneAndDelete({PARAM:req.params.PARAM}).then(() => res.json(true)).catch((err) => res.json(err))
                 })
             } else {throw new Error("MissingParamError: A parameter is required for delete functions")};
             break;
